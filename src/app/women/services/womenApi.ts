df03014d8db41abae2dc00a6e9442a529db62e42
Jéssyca Noronha <jessycarozeene@gmail.com>
2026-03-09 02:41:12 -0300

    🔧 build: deployment fix

--- src/app/women/services/womenApi.ts
+++ src/app/women/services/womenApi.ts
@@ -13,15 +13,37 @@ if (!BFF_BASE_URL) throw new Error("NEXT_PUBLIC_BFF_URL is not defined")
 const CONTENT_BASE_URL = process.env.NEXT_PUBLIC_CONTENT_URL
 if (!CONTENT_BASE_URL) throw new Error("NEXT_PUBLIC_CONTENT_URL is not defined")
 
-export async function getWomen(page = 1, limit = 12): Promise<WomenResponse> {
-  const url = `${BFF_BASE_URL}/api/women?page=${page}&limit=${limit}`
 
-  const res = await fetch(url)
-  if (!res.ok) {
-    const errData = await res.json().catch(() => null)
-    throw new Error(errData?.error || "Failed to fetch women data from BFF")
-  }
-  return res.json()
+/*
+@TODO Try to connect with BFF and deploy
+export async function getWomen(page = 1, limit = 12): Promise<WomenResponse> {
+ const url = `${BFF_BASE_URL}/api/women?page=${page}&limit=${limit}`
+
+ const res = await fetch(url)
+ if (!res.ok) {
+   const errData = await res.json().catch(() => null)
+   throw new Error(errData?.error || "Failed to fetch women data from BFF")
+ }
+ return res.json()
+ */
+
+export async function getWomen(page = 1, limit = 12) {
+  const res = await fetch(`${CONTENT_BASE_URL}/.json`);
+  if (!res.ok) throw new Error("Failed to fetch women data");
+
+  const dataRaw = await res.json();
+
+  const data: Woman[] = dataRaw ? Object.values(dataRaw.content) : [];
+
+  const start = (page - 1) * limit;
+  const paginated = data.slice(start, start + limit);
+
+  return {
+    data: paginated,
+    page,
+    total: data.length,
+    totalPages: Math.ceil(data.length / limit),
+  };
 }
 
 export async function getWomanDetail(order: number): Promise<Woman> {
